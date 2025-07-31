e16972a0fe769306da2a894910ede029979463d4
Targaryen-124 <ypadilla1988@gmail.com>
2025-07-30 20:19:38 -0600

    Mas
    Mas

--- src/inventariois/Factura.java
+++ src/inventariois/Factura.java
@@ -98,6 +98,7 @@ public class Factura extends javax.swing.JFrame {
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 890, 270));
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
